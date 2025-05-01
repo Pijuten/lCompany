cab4f7515eec8d9bf6b5b89f2576e67ba11191fc
stani <stani1423@gmail.com>
2025-05-01 16:09:15 +0200

    Add project files.

---
+++ lCompany/Main.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace lCompany
+{
+    [BepInPlugin("com.yourname.modname", "Mod Name", "1.0.0")]
+    public class ModName : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Harmony harmony = new Harmony("com.yourname.modname");
+            harmony.PatchAll();
+            Logger.LogInfo("Mod Name loaded.");
+        }
+    }
+}
